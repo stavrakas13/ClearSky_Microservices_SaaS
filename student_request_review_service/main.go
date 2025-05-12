1a80bc82ea4ef3a013168a0048f476e569029c35
ntua-el20082 <el20082@mail.ntua.gr>
2025-05-12 11:46:20 +0300

    added rabbit mq

--- student_request_review_service/main.go
+++ student_request_review_service/main.go
@@ -5,11 +5,19 @@ import (
 	"student_request_review_service/db"
 	"student_request_review_service/routes"
 
+	"os"
+	"student_request_review_service/mq"
+
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
 
 func main() {
+
+	os.Setenv("RABBITMQ_URL", "amqp://guest:guest@rabbitmq:5672/")
+
+	mq.InitRabbitMQ()
+	defer mq.MQChannel.Close()
 
 	db.InitDB()
 	defer db.CloseDB()
