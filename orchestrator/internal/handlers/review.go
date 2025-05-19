a5600a09e2d8297fbb498643305f8faf995fae59
ntua-el20082 <el20082@mail.ntua.gr>
2025-05-19 13:43:40 +0300

    started review.go

--- orchestrator/internal/handlers/review.go
+++ orchestrator/internal/handlers/review.go
@@ -4,8 +4,22 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-// handleReviewRequested processes grades.review.requested events
-func handleReviewRequested(d amqp.Delivery) {}
+// handleReviewRequested processes new request events
+// -> sends 2 events: student.postNewRequest & instructor.insertStudentRequest
+func handlePostNewRequest(d amqp.Delivery) {}
 
-// handleReviewResponded processes grades.review.responded events
-func handleReviewResponded(d amqp.Delivery) {}
+// handleGetRequestStatus processes student sees request status events
+// -> sends 1 event: student.getRequestStatus
+func handleGetRequestStatus(d amqp.Delivery) {}
+
+// handlePostResponse processes responses on review requests
+// -> sends 2 events: student.updateInstructorResponse & instructor.postResponse
+func handlePostResponse(d amqp.Delivery) {}
+
+// handleGetRequestList processes instructor get list of pending requests
+// -> sends 1 event: instructor.getRequestsList
+func handleGetRequestList(d amqp.Delivery) {}
+
+// handleGetRequestInfo processes instructor sees request details
+// -> sends 1 event: instructor.getRequestInfo
+func handleGetRequestInfo(d amqp.Delivery) {}
