11f5304834b758c4a5afafc6ee37f2f91e534256
el19954 <el19954@mail.ntua.gr>
2025-05-30 14:17:08 +0300

    minor db cahnges

--- stats_service/db/clearsky_schema.sql
+++ stats_service/db/clearsky_schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS grades;
 DROP TABLE IF EXISTS exams;
+DROP TABLE IF EXISTS grade_distributions;
 
 CREATE TABLE exams (
     exam_date TEXT NOT NULL,
@@ -19,14 +20,14 @@ CREATE TABLE grades (
     total_score FLOAT,
     PRIMARY KEY (exam_date, class_id, student_id),
     FOREIGN KEY (exam_date, class_id)
-        REFERENCES exams(exam_date, class_id)
+    REFERENCES exams(exam_date, class_id)
 );
 
 CREATE TABLE grade_distributions (
     class_id TEXT NOT NULL,
     exam_date TEXT NOT NULL,
     category TEXT NOT NULL,
-    value INT NOT NULL,
+    mark INT NOT NULL,
     count INT NOT NULL,
-    PRIMARY KEY (class_id, exam_date, category, value)
+    PRIMARY KEY (class_id, exam_date, category, mark )
 );
