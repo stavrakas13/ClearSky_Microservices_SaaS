ae5fba29378a3ebbf317013ec0de40bb474363df
ntua-el2084 <el20846@mail.ntua.gr>
2025-06-23 12:23:49 +0300

    db

--- instructor_review_reply_service/db/reviewsdb.sql
+++ instructor_review_reply_service/db/reviewsdb.sql
@@ -4,13 +4,13 @@ DROP TABLE IF EXISTS reviews;
 -- reviews made by students added here.
 CREATE TABLE IF NOT EXISTS reviews (
   review_id SERIAL PRIMARY KEY,
-  student_id VARCHAR(15) NOT NULL,
+  student_id VARCHAR(50) NOT NULL,
   course_id VARCHAR(50) NOT NULL,
-  exam_period VARCHAR(15) NOT NULL,
+  exam_period VARCHAR(50) NOT NULL,
   student_message TEXT NOT NULL,
-  status VARCHAR(15) DEFAULT 'pending' CHECK (status IN ('pending', 'reviewed')),
+  status VARCHAR(50) DEFAULT 'pending' CHECK (status IN ('pending', 'reviewed')),
   instructor_reply_message TEXT DEFAULT NULL,
-  instructor_action VARCHAR(25) DEFAULT NULL CHECK (instructor_action IN ('Total Accept', 'Will be considered', 'Denied')),
+  instructor_action VARCHAR(50) DEFAULT NULL CHECK (instructor_action IN ('Total Accept', 'Will be considered', 'Denied')),
   review_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   reviewed_at TIMESTAMP DEFAULT NULL
 );
--- student_request_review_service/db/reviewsdb.sql
+++ student_request_review_service/db/reviewsdb.sql
@@ -5,13 +5,13 @@ DROP TABLE IF EXISTS reviews;
 -- combination of student_id & course_id & exam_period UNIQUE for each review. 
 CREATE TABLE IF NOT EXISTS reviews (
   review_id SERIAL PRIMARY KEY,
-  student_id VARCHAR(15) NOT NULL,
+  student_id VARCHAR(50) NOT NULL,
   course_id VARCHAR(50) NOT NULL,
-  exam_period VARCHAR(15) NOT NULL,
+  exam_period VARCHAR(50) NOT NULL,
   student_message TEXT NOT NULL,
-  status VARCHAR(15) DEFAULT 'pending' CHECK (status IN ('pending', 'reviewed')),
+  status VARCHAR(50) DEFAULT 'pending' CHECK (status IN ('pending', 'reviewed')),
   instructor_reply_message TEXT,
-  instructor_action VARCHAR(25) DEFAULT NULL CHECK (instructor_action IN ('Total Accept', 'Will be considered', 'Denied')),
+  instructor_action VARCHAR(50) DEFAULT NULL CHECK (instructor_action IN ('Total Accept', 'Will be considered', 'Denied')),
   review_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   reviewed_at TIMESTAMP
 );
